fa413948b04d4201319ed81017f72158f31fcd16
Tanimal <Bobcheng.bob@gmail.com>
2024-06-12 00:05:00 +0800

    reweight

--- app/page.tsx
+++ app/page.tsx
@@ -9,23 +9,13 @@ import {
 } from "@/app/component/constant";
 
 export default function Home() {
-  function getSetting(pid: string, weight: boolean) {
-    setSetting({
-      pid: pid,
-      weight: weight,
-    });
-
-    if (weight) {
-      setStartWeight(true);
-      setStartTask(false);
-    } else {
-      setStartWeight(false);
-      setStartTask(true);
-    }
+  function reWeight() {
+    setStartWeight(true);
+    setStartTask(false);
   }
 
   function getWeight(weightDict: { [id: string]: number }) {
-    // uupdate weightResult
+    // update weightResult
     window.scroll({ top: 0, left: 0, behavior: "smooth" });
     setWeightResult(weightDict);
     setStartWeight(false);
@@ -39,16 +29,9 @@ export default function Home() {
     setTid(tid + 1);
   }
 
-  const [setting, setSetting] = useState<{
-    pid: string;
-    weight: boolean;
-  }>({
-    pid: "",
-    weight: false,
-  });
-
   const [startWeight, setStartWeight] = useState<boolean>(false);
   const [startTask, setStartTask] = useState<boolean>(false);
+  const [pid, setPid] = useState<string>("");
   const [tid, setTid] = useState<number>(1);
 
   const [weightResult, setWeightResult] = useState<{ [id: string]: number }>({
@@ -65,59 +48,72 @@ export default function Home() {
   );
 
   return (
-    <main className="flex min-h-screen flex-col gap-16 items-start p-8 md:flex-row">
-      <div className="flex flex-col gap-8 w-[28rem]">
+    <main className="flex min-h-screen flex-col p-8">
+      <div className="flex flex-row justify-between">
         <h1 className="text-2xl font-bold text-gray-800 mb-6">
           NASA-TLX Calculator
         </h1>
-        <TextInput placeholder="Participant ID (name)" pname="pid" />
-        <WeightTable {...weightResult} />
-        <ScoreTable data={scoreResult} weight={weightResult} />
+        <svg
+          className="size-8"
+          role="img"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>GitHub</title>
+          <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+        </svg>
       </div>
-      {setting.weight && startWeight ? (
-        <WeightForm sendToParent={getWeight} />
-      ) : null}
-      {startTask ? (
-        <div className="flex flex-col ">
-          <h2 className="text-xl font-bold text-gray-800 mb-6">Task {tid}</h2>
-          <ScoreForm sendToParent={getScore} />
+      <div className="flex flex-col gap-16 items-start md:flex-row">
+        <div className="flex flex-col gap-8 w-[32rem]">
+          <label className="bg-white max-w-56 relative block rounded-md border p-2 border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+            <input
+              type="text"
+              value={pid}
+              onInput={(e) => setPid(e.currentTarget.value)}
+              className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0"
+              placeholder="Participant Name"
+            />
+
+            <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+              Participant Name
+            </span>
+          </label>
+          <div className="flex flex-row items-end gap-8">
+            <WeightTable {...weightResult} />
+            <Button type="button" name="Reweight" handler={reWeight} />
+          </div>
+          <ScoreTable data={scoreResult} weight={weightResult} />
         </div>
-      ) : null}
+        {startWeight ? <WeightForm sendToParent={getWeight} /> : null}
+        {startTask ? (
+          <div className="flex flex-col ">
+            <h2 className="text-xl font-bold text-gray-800 mb-6">Task {tid}</h2>
+            <ScoreForm sendToParent={getScore} />
+          </div>
+        ) : null}
+      </div>
     </main>
   );
 }
 
 // Components
-interface TextInputProps {
-  placeholder: string;
-  pname: string;
-}
-function TextInput({ placeholder, pname }: TextInputProps) {
-  return (
-    <label className="bg-white max-w-56 relative block rounded-md border p-2 border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
-      <input
-        type="text"
-        name={pname}
-        className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0"
-        placeholder={placeholder}
-      />
-
-      <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
-        {placeholder}
-      </span>
-    </label>
-  );
-}
-
 interface BtnProps {
-  type: "button" | "submit" | "reset" | undefined;
+  type?: "button" | "submit" | "reset";
   name: string;
+  handler?: Function;
 }
-function Button({ type, name }: BtnProps) {
+function Button({ type, name, handler }: BtnProps) {
   return (
-    <span className="mt-4 w-fit inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm">
+    <span className="mt-4 h-fit w-fit inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm">
       <button
         type={type}
+        onClick={
+          handler
+            ? () => {
+                handler();
+              }
+            : undefined
+        }
         className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 focus:relative"
       >
         {name}
@@ -280,14 +276,6 @@ function WeightTable(data: { [id: string]: number }) {
             </tbody>
           </table>
         </div>
-        <span className="mt-4 h-fit w-fit inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm">
-          <button
-            type="button"
-            className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 focus:relative"
-          >
-            Reweight
-          </button>
-        </span>
       </div>
     </div>
   );
