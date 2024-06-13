3fa5d43c55d8ecb4424ad6b69a2dcf11d1d211d5
Tanimal <poyuncheng.bob@gmail.com>
2024-06-13 16:28:25 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NASA TLX Calculator by Tanimal",
+  title: "NASA TLX Calculator",
   description: "NASA TLX Calculator by Tanimal",
 };
 
